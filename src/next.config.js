725895e6642fa2745b6e53dde357f2097707a3e2
ShijiMi-Soup <takahiro.zt899@gmail.com>
2024-12-27 13:35:19 +0900

    Add offline support

--- src/next.config.js
+++ src/next.config.js
@@ -3,7 +3,10 @@ import nextPWA from "next-pwa"
 const withPWA = nextPWA({
   dest: "public",
   register: true,
-  skipWaiting: true
+  skipWaiting: true,
+  fallbacks: {
+    document: "/"
+  }
 })
 
 const nextConfig = withPWA({
