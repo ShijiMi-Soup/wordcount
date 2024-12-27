d9082c25f89a954465832f7def30d3df8b62a11d
ShijiMi-Soup <takahiro.zt899@gmail.com>
2024-12-27 13:43:03 +0900

    Update config

--- src/next.config.js
+++ src/next.config.js
@@ -4,9 +4,29 @@ const withPWA = nextPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  fallbacks: {
-    document: "/"
-  }
+  runtimeCaching: [
+    {
+      urlPattern: /\/_next\/static\//,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-resources",
+        expiration: {
+          maxAgeSeconds: 60 * 60 * 24 * 30 // Cache for 30 days
+        }
+      }
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|ico)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "image-assets",
+        expiration: {
+          maxEntries: 50, // Limit the number of cached images
+          maxAgeSeconds: 60 * 60 * 24 * 30 // Cache for 30 days
+        }
+      }
+    }
+  ]
 })
 
 const nextConfig = withPWA({
